545a24d9959e839178c25c4a4e5f19a7e54a5693
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2015-02-12 19:28:30 +0100

    Add postfix::map define

---
+++ manifests/map.pp
@@ -0,0 +1,44 @@
+# = Define:: postfix::map
+define postfix::map (
+    $path     = $title,
+    $confdir  = $postfix::params::confdir,
+    $ensure   = 'present',
+    $source   = undef,
+    $content  = undef,
+) {
+
+    if ! defined(Class['postfix']) {
+        fail('You must include the postfix base class before using any postfix defined resources')
+    }
+
+    if $path =~ /\// {
+        $real_path = $path
+    } else {
+        $real_path = "${confdir}/${path}"
+    }
+
+    file { $real_path:
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['postfix'],
+        notify  => Exec["postmap $real_path"],
+    }
+
+    # If the main.cf is handled via puppet, make sure that postmap command
+    # is executed after main.cf was created (because otherwise it fails)
+    if defined(Postfix::Config['/etc/postfix/main.cf']) {
+        Exec <| title == "postmap $real_path" |> {
+            require +> Postfix::Config['/etc/postfix/main.cf']
+        }
+    }
+
+    exec { "postmap $real_path":
+        command     => "/usr/sbin/postmap $real_path",
+        refreshonly => true,
+        require     => Package['postfix'],
+    }
+
+}
+    
