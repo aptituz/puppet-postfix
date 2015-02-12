1761e14fe229ec4b1d29fafb4f1b3ca11d98598f
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2015-02-12 19:12:13 +0100

    Add tests for further classes

---
+++ spec/classes/postfix_package.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'postfix' do
+        let (:facts) { {
+                :osfamily       => 'Debian'
+        }}
+                
+        context "basic tests" do
+                it { should contain_class('postfix::package') }
+                it { should contain_package('postfix') }
+        end
+end
