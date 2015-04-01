baeaade586c2c6c80796c36fd9500b0b5725c860
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2015-04-01 16:08:20 +0200

    Make ruby guys happy, by obeying their conventions

--- lib/puppet/provider/postconf_entry/postconf.rb
+++ lib/puppet/provider/postconf_entry/postconf.rb
@@ -1,48 +1,48 @@
 Puppet::Type.type(:postconf_entry).provide(:postconf) do
 
-    commands :postconf => 'postconf'
+  commands :postconf => 'postconf'
 
-    def self.instances
-        entries = postconf('-n')
-        entries.split("\n").collect do |line|
-            name, value = line.split(' = ', 2)
+  def self.instances
+    entries = postconf('-n')
+    entries.split("\n").collect do |line|
+      name, value = line.split(' = ', 2)
  
-            new(    :name   => name,
-                    :value  => value,
-                    :ensure => :present,
-            )
+      new(  :name   => name,
+          :value  => value,
+          :ensure => :present,
+      )
 
-        end
     end
+  end
 
-    def self.prefetch(lists)
-        instances.each do |prov|
-            if list = lists[prov.name] || lists[prov.name.downcase]
-                list.provider = prov
-            end
-        end
+  def self.prefetch(lists)
+    instances.each do |prov|
+      if list = lists[prov.name] || lists[prov.name.downcase]
+        list.provider = prov
+      end
     end
+  end
 
-    def exists?
-        @property_hash[:ensure] == :present
-    end
+  def exists?
+    @property_hash[:ensure] == :present
+  end
 
-    mk_resource_methods
+  mk_resource_methods
 
-    def value=(value)
-        postconf('-e', "#{resource[:name]}=#{value}")
-        @property_hash[:value] = value
-    end
+  def value=(value)
+    postconf('-e', "#{resource[:name]}=#{value}")
+    @property_hash[:value] = value
+  end
 
-    def create
-        postconf('-e', "#{resource[:name]}=#{resource[:value]}")
-        @property_hash[:value] = resource[:value]
-        @property_hash[:ensure] = :present
-    end
+  def create
+    postconf('-e', "#{resource[:name]}=#{resource[:value]}")
+    @property_hash[:value] = resource[:value]
+    @property_hash[:ensure] = :present
+  end
 
-    def destroy
-        debug("Destroying #{resource[:name]}")
-        postconf('-X', resource[:name])
-    end
+  def destroy
+    debug("Destroying #{resource[:name]}")
+    postconf('-X', resource[:name])
+  end
 
 end
