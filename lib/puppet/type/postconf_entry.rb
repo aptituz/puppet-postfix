a3812ee0d95f536ea72108798bda67b58caad63c
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2015-04-02 00:34:46 +0200

    Add some documentation

--- lib/puppet/type/postconf_entry.rb
+++ lib/puppet/type/postconf_entry.rb
@@ -1,18 +1,26 @@
 Puppet::Type.newtype(:postconf_entry) do
+  desc "Puppet type that allows setting postfix configuration settings"
   ensurable
 
   newparam(:name, :namevar => true) do
+    desc "the name of the configuration setting (used for uniqueness)"
   end
 
   newparam(:key) do
+    desc "the configuration key to be changed (defaults to name)"
     defaultto { @resource[:name] }
   end
 
   newproperty(:value) do
+    desc "which value to set on the specified key"
   end
 
   newparam(:confdir) do
+    desc "Optional: the confdir in which postfix configurations reside. Thats
+          most useful when working with multiple instances of postfix and
+          therefore defaults to '/etc/postfix'"
     defaultto '/etc/postfix'
+
 
     validate do |val|
       unless Pathname.new(val).absolute?
