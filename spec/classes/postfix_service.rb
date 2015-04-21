98e22a8eb15157623452ee1f32ad10b9deac7e19
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2015-04-21 15:41:45 +0200

    Change indenting of spec files

--- spec/classes/postfix_service.rb
+++ spec/classes/postfix_service.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'postfix' do
-        let (:facts) { {
-                :osfamily       => 'Debian'
-        }}
-                
-        context "basic tests" do
-                it { should contain_class('postfix::service') }
-                it { should contain_service('postfix') }
-        end
+  let (:facts) { {
+    :osfamily   => 'Debian'
+  }}
+    
+  context "basic tests" do
+    it { should contain_class('postfix::service') }
+    it { should contain_service('postfix') }
+  end
 end
--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -1,31 +1,32 @@
 require 'spec_helper'
 
 describe 'postfix' do
-        let (:facts) { {
-                :osfamily       => 'Debian'
-        }}
-                
-        context "basic tests" do
-                it { should contain_class('postfix') }
-                it { should contain_class('postfix::package') }
-                it { should contain_class('postfix::service') }
-                it { should contain_package('postfix') }
-                it { should contain_service('postfix') }
+  let (:facts) { {
+ 		:osfamily   => 'Debian'
+  }}
 
-        end
+  it { should compile.with_all_deps }
+    
+  context "basic tests" do
+    it { should contain_class('postfix') }
+    it { should contain_class('postfix::package') }
+    it { should contain_class('postfix::service') }
+    it { should contain_package('postfix') }
+    it { should contain_service('postfix') }
+  end
 
-        context "with manage_config => true" do
-                let (:params) {{ :manage_config => 'true' }}
+  context "with manage_config => true" do
+    let (:params) {{ :manage_config => 'true' }}
 
-                it { should contain_file('/etc/postfix/main.cf') }
-                it { should contain_file('/etc/postfix/master.cf') }
+    it { should contain_file('/etc/postfix/main.cf') }
+    it { should contain_file('/etc/postfix/master.cf') }
 
-                context "with postfix_options set" do
-                    let (:params) {{ 
-                        :manage_config   => 'true',
-                        :postfix_options => { 'myhostname' => 'foobar' }}}
+    context "with postfix_options set" do
+      let (:params) {{ 
+      :manage_config   => 'true',
+      :postfix_options => { 'myhostname' => 'foobar' }}}
 
-                    it { should contain_file('/etc/postfix/main.cf').with_content(/\nmyhostname = foobar\n/) }
-                end
-        end
+      it { should contain_file('/etc/postfix/main.cf').with_content(/\nmyhostname = foobar\n/) }
+    end
+  end
 end
