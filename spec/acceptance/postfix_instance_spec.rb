fea1cf977aa30d50f7433d8b050523fff7e60687
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2015-04-21 17:08:11 +0200

    Add some tests for postfix_instance

--- spec/acceptance/postfix_instance_spec.rb
+++ spec/acceptance/postfix_instance_spec.rb
@@ -1,29 +1,44 @@
 require 'spec_helper_acceptance'
+require 'pry'
 
 describe 'postfix_instance' do
+  before :all do
+    # include postfix class for package installation
+    apply_manifest('include postfix', :catch_failures => true)
+  end
+  
   describe "getting a list of instances via puppet ressource" do
     describe command('puppet resource postfix_instance') do
       its(:stdout) { should match /postfix_instance { '-':/ }
     end
   end
   
-describe 'running puppet code' do
-  describe 'should run with no errors' do
+  describe 'running puppet code' do
+    it 'should run with no errors' do
       pp = <<-EOS
-        postfix_instance { 'postfix-out': }
+        postfix_instance { 'postfix-out': ensure => present }
       EOS
 
-#FIXME: This code does not work, since it causes beaker not to install
-#the module, before. WTF?
-#		apply_manifest(pp, :catch_failures => true)
-#    apply_manifest(pp, :catch_changes => true)
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
   end
-end   
-
-##      apply_manifest(pp, :catch_failures => true)
-##      apply_manifest(pp, :catch_changes => true)
-##    end      
-##  end
   
+  it 'should create instance' do
+    shell('postmulti -l|grep postfix-out', :acceptable_exit_codes => 0)
+  end
+
+  describe file('/etc/postfix-out') do
+    it { should be_directory }
+  end
+   
+  describe file('/etc/postfix-out/main.cf') do
+    it { should be_file }
+  end
+  
+  
+  describe file('/etc/postfix-out/master.cf') do
+    it { should be_file }
+  end
 end
   
