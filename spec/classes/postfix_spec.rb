0db5984d586d23c5e520d3faf1c54bcc6ee0703e
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2017-11-02 14:56:29 +0100

    Fix indentation in specs

--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe 'postfix' do
   let (:facts) { {
- 		:osfamily   => 'Debian'
+    :osfamily   => 'Debian'
   }}
 
   it { should compile.with_all_deps }
-    
+
   context "basic tests" do
     it { should contain_class('postfix') }
     it { should contain_class('postfix::package') }
@@ -22,7 +22,7 @@ describe 'postfix' do
     it { should contain_file('/etc/postfix/master.cf') }
 
     context "with postfix_options set" do
-      let (:params) {{ 
+      let (:params) {{
       :manage_config   => 'true',
       :postfix_options => { 'myhostname' => 'foobar' }}}
 
--- spec/defines/postfix_map_spec.rb
+++ spec/defines/postfix_map_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 
 describe 'postfix::map' do
-        let :pre_condition do
-            'class { "postfix": manage_config => false }'
-        end
+  let :pre_condition do
+    'class { "postfix": manage_config => false }'
+  end
 
-        let(:title) { 'transports' }
-                
-        context "basic tests" do
-             it { should contain_file('/etc/postfix/transports').with({
-                'owner'   => 'root',
-                'group'   => 'root',
-                'mode'    => '0644'
+  let(:title) {'transports'}
 
-             })}
-             it { should contain_exec('postmap /etc/postfix/transports') }
+  context "basic tests" do
+    it { should contain_file('/etc/postfix/transports').with({
+        'owner' => 'root',
+        'group' => 'root',
+        'mode' => '0644'
 
-        end
+    })}
+    it { should contain_exec('postmap /etc/postfix/transports')}
+  end
 end
